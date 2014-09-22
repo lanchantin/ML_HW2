660c4cf75d88777b7b30f0033d4ed98deb2d5be7
Jack Lanchantin <jacklanchantin@gmail.com>
2014-09-22 15:28:14 -0400

    added cv method

--- ridgeRegression.py
+++ ridgeRegression.py
@@ -7,18 +7,21 @@ import matplotlib.pyplot as pl
 def loadDataSet(dataFile):
 	xVal = np.asmatrix(np.loadtxt(dataFile, delimiter = " ", usecols=(0,1),unpack=True)).transpose()
 	yVal = np.asmatrix(np.loadtxt(dataFile, delimiter = " ", usecols=(2,),unpack=True)).transpose()
-	# pl.plot(xVal[1],yVal,'ro')
-	# pl.xlabel(r'$X_1$')
-	# pl.ylabel('Y')
-	# pl.title('Question 4 - Part A')
-	# pl.legend('Y',loc=2)
-	# pl.show()
 
 	return (xVal,yVal)
 
 
-def normalEquations(x,y):
-
-	theta = (x.T*x)*(x.T*y)
-
+def ridgeRegress(x,y,L):
+	I = np.identity(x[0].size) 
+	theta = ((x.T*x)+L*I)*(x.T*y)
 	return theta
+
+
+def cv(x,y):
+	kFold = len(x)/10
+	L = 0.2
+	for i in range(0, 10):
+		Bk = ridgeRegress(x[currFold],y[currFold],L)
+
+
+	#use Bk on test 
