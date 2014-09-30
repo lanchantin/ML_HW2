009eea07b8b44eac6a73b01f4613c1366470dfca
Jack Lanchantin <jacklanchantin@gmail.com>
2014-09-29 23:45:28 -0400

    ridge

--- ridgeRegression.py
+++ ridgeRegression.py
@@ -7,23 +7,20 @@ dataFile = 'RRdata.txt'
 
 
 def loadDataSet(dataFile):
-	xval = np.loadtxt(dataFile, delimiter = " ", usecols=(0,1,2),unpack=True).transpose()
-	yval = np.loadtxt(dataFile, delimiter = " ", usecols=(3,),unpack=True).transpose()
-
+	xval = np.loadtxt(dataFile, delimiter = " ", usecols=(0,1,2))
+	yval = np.loadtxt(dataFile, delimiter = " ", usecols=(3,))
 	return (xval,yval)
 
 
 def ridgeRegress(x,y,L):
 	I = np.identity(x[0].size) 
-	theta = np.dot((np.linalg.inv(np.dot(x.T,x)+L*I)),np.dot(x.T,y))
+	theta = np.dot((np.linalg.inv(np.dot(x.T,x)+(L*I))),np.dot(x.T,y))
 	return theta
 
 # Bk = ridgeRegress(xval,yval,0)
 # Z = np.dot(xval,Bk)
 # X, Y = np.meshgrid(x[:,1], x[:,2])
 # ax.plot_surface(X, Y, yval)
-
-
 
 def drange(start, stop, step):
 	r = start
@@ -32,43 +29,47 @@ def drange(start, stop, step):
 		r += step
 
 def cv(x,y):
-	kFold = len(x)/10
-	lowSum = 0
+	kFolds = len(x)/10
+	lowLoss = 0
 	bestL = 0
 
-	random.seed(30)
+	random.seed(37)
 	random.shuffle(x)
 	random.shuffle(y)
 
-	for L in drange(0,1.02,0.02):
-		sum = 0
+	for L in drange(0.2,1.02,0.02):
+		loss = 0
 		for i in range(0,10):
-			testFold = i*kFold
-			a = x[0:testFold]
-			b = x[testFold+kFold:x.shape[0]]
+			currFold = i*kFolds
+
+			a = x[0:currFold]
+			b = x[currFold+kFolds:x.shape[0]]
 			xTrain = np.concatenate((a,b),axis = 0)
 
-			a = y[0:testFold]
-			b = y[testFold+kFold:y.shape[0]]
+			a = y[0:currFold]
+			b = y[currFold+kFolds:y.shape[0]]
 			yTrain = np.concatenate((a,b),axis = 0)
+
+			xTest = x[currFold:(currFold+kFolds)]
+			yTest = y[currFold:(currFold+kFolds)]
 
 			Bk = ridgeRegress(xTrain,yTrain,L)
 
-			y_hat = np.dot(x[testFold:(testFold+kFold)],Bk)
+			y_hat = np.dot(xTest,Bk)
 
-			for j in range(0,kFold):
-				sum = sum + math.pow((y[testFold+j] - y_hat[j]),2)
-		print sum
+			for j in range(0,kFolds):
+				loss = loss + math.pow((yTest[j] - y_hat[j]),2)
+				# print loss
 
-		if (L == 0):
-			lowSum = sum
-			#print 'lowsum: ' + str(lowAvg)
+		loss = loss/10
+		print loss
+		if (L == 0.2):
+			lowLoss = loss
+			bestL = L
 		else:
-			#print 'if ' + str(sum) + '<' + str(lowAvg)
-			if sum < lowSum:
-				lowSum = sum
+			if loss < lowLoss:
+				lowLoss = loss
 				bestL = L
-		print sum
 
 	return bestL
 
